626ba8b9e5c70e21dc6aab728b0add7559ec51d2
Bezahun Asrat Bekele <bezahun33@gmail.com>
2022-06-30 13:32:21 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf( "with proper grammar, but the outcome is a piece of art,\n" );
+#include <stdio.h>
+
+/**
+*main - prints the size of various types
+*Return: 0 if existed properly, non-zero if otherwise
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
