138e4af6998fdef38626a352a3c2204e67e0d2c8
Bezahun Asrat Bekele <bezahun33@gmail.com>
2022-06-30 14:04:53 -0700

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+*main - prints the size of different types
+*Return: 1 if exited properly, non-one if otherwise
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
