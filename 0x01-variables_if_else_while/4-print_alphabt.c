a6518eba5c7d5e7c106f49b6a361fcac3ceea800
Bezahun Asrat Bekele <bezahun33@gmail.com>
2022-07-04 01:53:21 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,15 @@ int main(void)
 int ch;
 
 for (ch = 'A'; ch <= 'D'; ch++)
+{
 for (ch = 'F'; ch <= 'P'; ch++)
+{
 for (ch = 'R'; ch <= 'Z'; ch++)
 {
 int lower_ch = tolower(ch);
 putchar(lower_ch);
+}
+}
 }
 putchar('\n');
 
