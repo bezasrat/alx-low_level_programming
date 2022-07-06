65a129a76d049e67e5dbeda786e28b380c72fbdd
Bezahun Asrat Bekele <bezahun33@gmail.com>
2022-07-06 11:08:46 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int year)  
+void print_remaining_days(int month, int day, int year)
 {
 if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 {
